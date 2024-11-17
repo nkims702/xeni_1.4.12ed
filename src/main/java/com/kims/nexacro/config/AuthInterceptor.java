9acbcd5b62ae76a37dd92d5c3ecaddadbfc1a6c5
nkims702 <80798564+nkims702@users.noreply.github.com>
2024-11-17 16:14:23 +0900

    .do 찾기

--- src/main/java/com/kims/nexacro/config/AuthInterceptor.java
+++ src/main/java/com/kims/nexacro/config/AuthInterceptor.java
@@ -29,11 +29,34 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
     		try {
     			String fileName = "";
         		String fileNameExtension = "";
+        		
+        		System.out.println("find fileName : " + fileName + ", fileNameExtension : " + fileNameExtension + ", 확장자 찾기 :" + txtExt);
+    			
         		StringTokenizer st = new StringTokenizer(txtExt, ".");
-        		while( st.hasMoreTokens()) {
-        			fileName = st.nextToken();
-        			fileNameExtension = st.nextToken();
-        		}
+        		try {
+        			int tokensIndx = 0;
+        			
+        			int tokensSize = st.countTokens();
+        			String[] tokenArray = new String[tokensSize];
+	        		while( st.hasMoreTokens()) {
+//	        			if(tokensIndx == 0 ) {
+//	        				fileName = st.nextToken();
+//	        			}
+//	        			fileNameExtension = st.nextToken();
+	        			
+	        			for( int i = 0 ; i < tokensSize; i++) {
+	        				String _strTokensValue = st.nextToken();
+	        				System.out.println("tokens size : "+tokensSize+" tokens ["+i+"] : " + _strTokensValue);
+	        				tokenArray[i] = _strTokensValue;
+	        			}
+	        			
+	        			System.out.println("확장자 #########################################################################################[ " + tokenArray[tokensSize-1] + " ]");
+	        			
+	        		}
+        		}catch (Exception e) {
+        			e.getStackTrace();
+        			System.out.println(e.getMessage());
+				}
         		
         		if( !fileNameExtension.equals("") &&fileNameExtension.equals("do")) {
         			System.out.println("find do fileName : " + fileName + ", fileNameExtension : " + fileNameExtension + ", 확장자 찾기 :" + txtExt);
@@ -51,6 +74,8 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
         		            // 로그인하지 않은 사용자일 경우 로그인 페이지로 이동
         		            //response.sendRedirect("/error");
         		            //return false;
+        		            
+        		            throw new Exception("current user is not logined");
         		        }else {
         		        	System.out.println("current user is logined");
         		        	
@@ -63,7 +88,8 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
         		}
     			
     		}catch (Exception e) {
-				// TODO: handle exception
+    			e.getStackTrace();
+    			throw new Exception(e);
 			}
     		
     	}
